0963df0426dc42c6a141ef0f4d44865209596a6a
gustav <gustav.albrektson@gmail.com>
2024-03-05 20:51:03 +0100

    Add page header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,23 @@
+import "./Header.css";
+
+const Button = (props) => {
+    return (
+        <div className={"button " + props.name}>
+            <p>{props.text}</p>
+        </div>
+    );
+}
+
 const Header = () => {
     return (
-        <div>
-            <h1>Boka</h1>
+        <div className="header-container">
+            <div className="header">
+                <img className="logo" src={require("img/logo.png")} alt="Örni"/>
+                <div className="header-buttons">
+                    <Button name="spotify" text="Spotify"/>
+                    <Button name="booking" text="Boka" />
+                </div>
+            </div>
         </div>
     );
 }
