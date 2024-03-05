efa45603dc1714e58d6202fad7d3557d07a800f4
gustav <gustav.albrektson@gmail.com>
2024-03-05 19:56:38 +0100

    Initial commit

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import Header from "components/Header";
+
+const Home = () => {
+    return (
+        <div>
+            <Header />
+            <h1>Örni</h1>
+            <p>Bra musik!!</p>
+        </div>
+    );
+}
+
+export default Home;
