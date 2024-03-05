35bf89f2e016e23d5774572087dae920c0618923
gustav <gustav.albrektson@gmail.com>
2024-03-05 21:01:42 +0100

    Update header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,12 @@
 import "./Header.css";
 
 const Button = (props) => {
+    const handleClick = () => {
+        window.open(props.url, "_blank");
+    }
+    
     return (
-        <div className={"button " + props.name}>
+        <div className={"button " + props.name} onClick={handleClick}>
             <p>{props.text}</p>
         </div>
     );
@@ -14,8 +18,8 @@ const Header = () => {
             <div className="header">
                 <img className="logo" src={require("img/logo.png")} alt="Örni"/>
                 <div className="header-buttons">
-                    <Button name="spotify" text="Spotify"/>
-                    <Button name="booking" text="Boka" />
+                    <Button name="spotify" text="Spotify" url="https://open.spotify.com/artist/5g4goXzUuMiUvgvgxBAMtA?si=QGyl1O5dSMajGi_jpeid2Q"/>
+                    <Button name="booking" text="Boka" url="https://l.facebook.com/l.php?u=http%3A%2F%2Fwww.mcv.se%2F&h=AT12d66r2xdMqjJdPDw_e081wmyHI13X9CKOV40UHpi8yUhXrBeek5HciPoa-4H2YwhASiMFtKrhNjoHfNkR4s7_QojyU7aTa2fBZpkf47FfHjMMGCRJz95ZZoM"/>
                 </div>
             </div>
         </div>
