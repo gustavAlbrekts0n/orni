6e66a3a495527c6f4efec57e19937073feb2428c
gustav <gustav.albrektson@gmail.com>
2024-03-07 20:37:34 +0100

    Add social links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,26 @@
 import "./Footer.css";
 
+const Social = (props) => {
+    const handleClick = () => {
+        window.open(props.url, "_blank");
+    }
+
+    return (
+        <div className={"social social-" + props.name}  onClick={handleClick}>
+            <p>{props.text}</p>
+        </div>
+    );
+}
+
 const Footer = () => {
     return (
         <div className="footer-container">
             <div className="footer">
+                <div className="social-links">
+                    <Social name="spotify" text="Spotify" url="https://open.spotify.com/artist/5g4goXzUuMiUvgvgxBAMtA?si=QGyl1O5dSMajGi_jpeid2Q"/>
+                    <Social name="facebook" text="Facebook" />
+                    <Social name="youtube" text="YouTube" />
+                </div>
                 <p>&copy; 2024 Örni.</p>
             </div>
         </div>
