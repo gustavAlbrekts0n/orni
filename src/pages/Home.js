565a1822a47d9b6961d78a6d79c866730e25905e
gustav <gustav.albrektson@gmail.com>
2024-03-07 18:54:03 +0100

    Add intro text

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,8 +8,20 @@ const Home = () => {
             <Header />
             <div className="body">
                 <img className="img-trio" src={require("img/background.jpg")} alt="Bild på trion"/>
-                <h1>Örni</h1>
-                <p>Bra musik!!</p>
+                <div className="intro">
+                    <div className="intro-body">
+                        <h1>Örni</h1>
+                        <p>
+                        Rock som spritter, skevar och drar med egensinniga svenska texter. Mjukt möter hårt – som regn mot mark.
+                        </p>
+                        <p>
+                        <strong>Bob Hund</strong>, <strong>November</strong> och <strong>Queens of the Stone Age</strong> är alla referenser som nämnts i samma andetag som Örni. Och, visst, allt stämmer.
+                        </p>
+                        <p>
+                        Men Örni är framför allt väldigt, väldigt egna.
+                        </p>
+                    </div>
+                </div>
                 <SpotifyEmbeds />
             </div>
         </div>
