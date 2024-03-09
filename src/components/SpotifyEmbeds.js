a9315336d507fd287924689bbe0178c7472ab97d
gustav <gustav.albrektson@gmail.com>
2024-03-09 17:14:34 +0100

    Spotify embeds text

--- src/components/SpotifyEmbeds.js
+++ src/components/SpotifyEmbeds.js
@@ -4,6 +4,7 @@ import "./SpotifyEmbeds.css";
 const SpotifyEmbeds = () => {
     return (
         <div className="container">
+            <p>Lyssna nu!</p>
             <Spotify className="embed" wide link="https://open.spotify.com/track/7GettAr7UidDKaTlLN3flD?si=4608748906384ce8" />
             <Spotify className="embed" wide link="https://open.spotify.com/track/1CiArdHpPaTvyInGmKoaiR?si=0731eb90ec5644a0" />
             <Spotify className="embed" wide link="https://open.spotify.com/track/39rewKEkB8CpCMA2Ube7Gm?si=a1283ad9e7e34d63" />
